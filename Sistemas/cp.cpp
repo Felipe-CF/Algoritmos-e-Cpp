ab93429d11543d72f6131a7fa6242e59c12826ca
Felipe-CF <132963984+Felipe-CF@users.noreply.github.com>
2024-03-17 20:19:52 +0000

    fazendo o cp

---
+++ Sistemas/cp.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstring>
+#include <string>
+using namespace std;
+
+int main(int argc, char* argv[]){
+    //char s = argv[1];
+    string s(argv[1]);
+    cout << s.length() << endl;
+
+}
