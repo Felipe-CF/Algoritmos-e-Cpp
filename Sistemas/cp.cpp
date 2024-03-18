e598c6fcfe8df37aaa719415b0a61cfd2101a743
Felipe-CF <132963984+Felipe-CF@users.noreply.github.com>
2024-03-18 11:40:11 +0000

    Fazendo cp

--- Sistemas/cp.cpp
+++ Sistemas/cp.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 #include <cstring>
 #include <string>
+#include <fstream>
 using namespace std;
 
 int main(int argc, char* argv[]){
-    //char s = argv[1];
-    string s(argv[1]);
-    cout << s.length() << endl;
+
+    if(argc == 1)
+        cout << "cp: missing file operand\n" << "Try 'cp --help' for more information." << endl;
+    else{
+        std::ifstream origem;
+        origem.open(argv[1]);
+    }
+    // string s(argv[0]);
+
 
 }
